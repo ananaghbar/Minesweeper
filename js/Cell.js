d46884c8b6d80553c616713ba3cd43cf425c5a81
ananaghbar <anan.aghbar@hotmail.com>
2017-09-09 18:38:44 +0300

    open the clicked cell

--- js/Cell.js
+++ js/Cell.js
@@ -7,4 +7,48 @@ var Cell = function(element,x,y){
 	this.isFlagged = false;
 	this.isEmpty = false;
 	this.mineCount = 0;
+
+
+	/*Set a mine in the cell*/
+	this.setMine = function(){
+		this.isMine=true;
+	}
+
+	/*set the open flag**/
+	this.setOpened = function(){
+		this.isOpened=true;
+		//to change the style of the cell 
+		this.element.classList.add('is-opened');
+	}
+	/*set the flagged flag*=>right clicked*/
+	this.setFlagged = function(){
+		this.isFlagged=true;
+		//to change the style of the cell 
+		this.element.classList.add('fa');
+		this.element.classList.add('fa-flag');
+	}
+   	this.setUnflagged = function(){
+
+      this.isFlagged = false;
+      this.element.classList.remove('fa');
+      this.element.classList.remove('fa-flag');
+   	}
+
+	this.open = function (){
+
+      this.setOpened();
+
+      if (this.isMine) {
+         this.element.classList.add('fa');
+         this.element.classList.add('fa-bomb');
+         return this.element.classList.add('is-mine');
+      }
+
+      if (this.isEmpty) {
+         return this.element.classList.add('is-empty');
+      }
+
+      //set the number of the sibllings
+      this.element.textContent = this.mineCount;		
+	}
 }
